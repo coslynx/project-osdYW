06984076b5a732673c9c17b9d790beaf0c5e2c85
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-29 20:01:42 +0530

    Added: `discord-moderation-bot/automated-responses/responses.js`

---
+++ discord-moderation-bot/automated-responses/responses.js
@@ -0,0 +1,10 @@
+const responses = {
+  greeting: "Hello! How can I assist you today?",
+  goodbye: "Goodbye! See you next time.",
+  help: "If you need help, please use the command !help to see available options.",
+  report: "Thank you for reporting this issue. Our team will look into it promptly.",
+  assistance: "Please wait a moment. A moderator will assist you shortly.",
+  automatedResponse: "I'm sorry, but I am a bot and cannot provide personalized responses. Please use !help for assistance.",
+};
+
+module.exports = responses;
