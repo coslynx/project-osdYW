76690a0ab032d446e96a2c4313a41a5e37a6e07b
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-05-29 20:02:07 +0530

    Added: `discord-moderation-bot/enhancements/reporting-system/report.js`

---
+++ discord-moderation-bot/enhancements/reporting-system/report.js
@@ -0,0 +1,16 @@
+const report = {
+  createReport: (user, message) => {
+    // Logic to create a report
+  },
+  submitReport: (report) => {
+    // Logic to submit a report
+  },
+  getReports: () => {
+    // Logic to fetch reports
+  },
+  deleteReport: (reportId) => {
+    // Logic to delete a report
+  },
+};
+
+module.exports = report;
